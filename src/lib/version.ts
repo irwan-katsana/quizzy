de15a97ccccc858fdac10caae5fbba568402c475
irwan6179 <m.irwan@gmail.com>
2024-11-26 14:40:39 +0800

    bump test

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 // Version management for the application
-export const VERSION = 'v0.4.1';
+export const VERSION = 'v0.4.2';
 
 // Helper function to get version with prefix
 export const getVersion = () => VERSION;
